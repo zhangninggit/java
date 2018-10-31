542bd539a0714fc331a0d43328b8a14f92ca2a67
xiaoning <zhangning2017@163.com>
2018-10-31 22:03:30 +0800

    监听事件ActionListener (复制第一个文本框中的内容到第二个文本框) 事件源触发ActionEvent事件后，监视器调用接口方法中的actionPerformed对发生的事件作出处理

---
+++ src/MyFrame2.java
@@ -0,0 +1,39 @@
+import javax.swing.*;
+import java.awt.event.*;
+import java.awt.*;
+
+
+public class MyFrame2 extends JFrame{
+    JTextField tf1,tf2;
+    JButton bt;
+    Handler hd;
+    public MyFrame2(String s){
+        super(s);
+        this.setBounds(10,20,250,300);
+        setLayout(new FlowLayout());
+        tf1 = new JTextField(20);
+        bt = new JButton("copy");
+        tf2 = new JTextField(20);
+        this.add(tf1);
+        this.add(bt);
+        this.add(tf2);
+        hd = new Handler();
+        bt.addActionListener(hd);
+        //this.add(tf1);
+        this.setVisible(true);
+
+        this.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+    }
+
+    class Handler implements ActionListener{
+        public void actionPerformed(ActionEvent e){
+            String s=tf1.getText();
+            tf2.setText(s);
+            tf1.setText("");
+        }
+    }
+
+    public static void main(String[] args) {
+        new MyFrame2("监听事件");
+    }
+}

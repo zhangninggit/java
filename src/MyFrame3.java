5a284979d60449297ae701e51432a18d52f1a21e
xiaoning <zhangning2017@163.com>
2018-11-03 00:21:47 +0800

    捕获鼠标位置

--- src/MyFrame3.java
+++ src/MyFrame3.java
@@ -1,2 +1,38 @@
-public class MyFrame3 {
+import javax.swing.*;
+import java.awt.*;
+import java.awt.event.MouseEvent;
+import java.awt.event.MouseMotionListener;
+
+//获取鼠标的位置，设置一个监听事件
+public class MyFrame3 extends JFrame{
+    JTextField tf;
+    Handler hd;
+
+    MyFrame3(String s){
+        super(s);
+        setBounds(0,0,400,400);
+        tf =new JTextField(20);
+        setLayout(new FlowLayout());
+        add(tf);
+        hd =new Handler();
+        addMouseMotionListener(hd);
+        setVisible(true);
+        setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+    }
+
+    class Handler implements MouseMotionListener{
+        public void mouseMoved(MouseEvent e){
+
+        }
+
+        public void mouseDragged(MouseEvent e){
+            int x = e.getX();
+            int y = e.getY();
+            tf.setText("x:"+x+"   "+"y:"+y);
+        }
+    }
+
+    public static void main(String[] args) {
+        new MyFrame3("获取鼠标坐标");
+    }
 }
